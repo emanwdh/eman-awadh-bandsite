f2d1c247f3c59a878e83c2d8a6d4674f124f59c4
Eman Awadh <emanwdh@gmail.com>
2024-07-05 21:33:58 -0400

    Build JS: Build Relative Time Stamp for posted comments on bio page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -22,6 +22,51 @@
 
 const BioInstance = new BandSiteApi();
 
+
+
+//relative time function
+
+function relativeDate(date) {
+
+    const diff = Math.round ((new Date() - new Date(date))/ 1000);
+    const minute = 60; 
+    const hour = minute * 60;
+    const day = hour * 24; 
+    const week = day * 7; 
+    const month = day * 30;
+    const year = month * 12;
+
+
+  if (diff < 30) {
+    return "just now";
+  } else if (diff < minute) {
+    return diff + " seconds ago";
+  } else if (diff < 2 * minute) {
+    return "a minute ago";
+  } else if (diff < hour) {
+    return Math.floor(diff / minute) + " minutes ago";
+  } else if (Math.floor(diff / hour) == 1) {
+    return "1 hour ago";
+  } else if (diff < day) {
+    return Math.floor(diff / hour) + " hours ago";
+  } else if (diff < day * 2) {
+    return "yesterday";
+  } else if (diff < week) {
+    return week + " days ago";
+  } else if (diff < month) {
+    return Math.floor(diff / week) + " weeks ago";
+  } else if (diff < year) {
+    return Math.floor(diff / month) + " months ago";
+  } else {
+    return Math.floor(diff / year) + " years ago";
+  }
+
+
+};
+
+
+
+
 // async function to display comments 
 
 async function displayComments () {
@@ -56,7 +101,7 @@ async function displayComments () {
         oldCommentContainer.appendChild(oldCommentDate);
         let oldDateValue = commentArray[i].timestamp;
         let oldDateStringValue = new Date(oldDateValue);
-        oldCommentDate.textContent = oldDateStringValue.toLocaleDateString();
+        oldCommentDate.textContent = relativeDate(oldDateStringValue);
 
         
 
@@ -116,7 +161,6 @@ async function addComment(event){
 
         let commentArray = await BioInstance.getComments();
         let newDate = commentArray[(commentArray.length-1)].timestamp;
-        let newDateString = new Date(newDate);
 
         let newCommentContainer = document.createElement('div');
         newCommentContainer.classList.add("comment");
@@ -144,7 +188,7 @@ async function addComment(event){
         let newCommentDate = document.createElement('p');
         newCommentDate.classList.add("comment__date");
         newCommentContainer.appendChild(newCommentDate);
-        newCommentDate.textContent = newDateString.toLocaleDateString();
+        newCommentDate.textContent = relativeDate(newDate);
 
         event.target.reset();
         
@@ -154,5 +198,7 @@ async function addComment(event){
 };
 
 formElement.addEventListener("submit", addComment);
+
+
 
 
